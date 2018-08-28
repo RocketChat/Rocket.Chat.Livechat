4eaef56a0503b0d55b7b25ebbc3fb46a2e2d0fad
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-08-28 17:58:16 -0300

    Center tooltip in storybook

--- src/components/tooltip/stories.js
+++ src/components/tooltip/stories.js
@@ -1,10 +1,12 @@
 import { h } from 'preact';
 import { storiesOf } from '@storybook/react';
+import centered from '@storybook/addon-centered';
 import { Tooltip } from './index';
 
 const tooltipText = 'Tooltip text';
 
 storiesOf('Tooltip', module)
+	.addDecorator(centered)
 	.add('without arrowhead', () => <Tooltip>{tooltipText}</Tooltip>)
 	.add('with arrowhead on top', () => <Tooltip arrowhead="top">{tooltipText}</Tooltip>)
 	.add('with arrowhead on bottom', () => <Tooltip arrowhead="bottom">{tooltipText}</Tooltip>)
