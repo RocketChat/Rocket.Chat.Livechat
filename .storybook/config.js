595faa1badebddafb89c82c3a1e47673d1ca5220
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-08-28 17:03:42 -0300

    Add storybook knobs and options addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,19 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { setOptions } from '@storybook/addon-options';
+
+setOptions({
+  name: 'RocketChat Livechat',
+	url: 'https://github.com/RocketChat/Rocket.Chat.Livechat',
+  hierarchySeparator: /\//,
+  hierarchyRootSeparator: /\|/,
+});
 
 function loadStories() {
+	addDecorator(withKnobs);
 	require('../stories/index.js');
-	// You can require as many stories as you need.
+	const req = require.context('../src/components', true, /(story|stories)\.js$/);
+	req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
--- stories/index.js
+++ stories/index.js
@@ -1,13 +1,1 @@
-import { h } from 'preact';
-import { setOptions } from '@storybook/addon-options';
 import '../src/styles';
-
-function importAll(r) {
-	r.keys().forEach((key) => r(key));
-}
-
-setOptions({
-	hierarchySeparator: '\\.',
-});
-
-importAll(require.context('../src', true, /story$/));
