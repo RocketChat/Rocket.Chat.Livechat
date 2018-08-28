831e012fa756d6b59f048504d9e54a3f232a81c5
Guilherme Gazzo <guilhermegazzo@gmail.com>
2018-08-28 15:35:02 -0300

    Composer component

---
+++ src/components/Composer/index.js
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+import style from './style';
+const getStyles = (style, name, classes) => [style[name], ...Object.entries(style).filter(([key]) => classes[key]).map(([, value]) => value)].join(' ');
+
+const Composer = ({ pre, post, placeholder, ...args }) => (
+	<div {...args} className={getStyles(style, 'composer', {})}>
+		{ pre }
+		<div placeholder={placeholder} className={getStyles(style, 'input', {})} contenteditable>asd</div>
+		{ post }
+	</div>);
+
+export default Composer;
