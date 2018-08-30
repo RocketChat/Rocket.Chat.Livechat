acf442b9bd6f2adfdb802361099955bfb8652e2f
Guilherme Gazzo <guilhermegazzo@gmail.com>
2018-08-30 14:51:37 -0300

    Avatar component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,14 @@
 import { h } from 'preact';
 import style from './style';
 
-const Header = ({ children }) => (
-	<header class={style.header}>
-		<h1>{children}</h1>
+const Header = ({ children, ...args }) => (
+	<header {...args} class={style.header}>
+		{children}
 	</header>
 );
 
+
+export const Content = ({ children }) => <div class={style.content}>{children}</div>;
+export const Title = ({ children }) => <div class={style.title}>{children}</div>;
+export const SubTitle = ({ children }) => <div class={style.subTitle}>{children}</div>;
 export default Header;
