619e08f418faa102e6bf02852441254095166e69
Tasso Evangelista <tasso@tassoevan.me>
2018-09-12 11:47:32 -0300

    [NEW] Tooltip component (#25)
    * Add helper to create classes from styles
    
    * Add storybook addon centered
    
    * Add storybook knobs and options addon
    
    * Move storybook configuration
    
    * Remove dangling styles directory
    
    * Add Tooltip component
    
    * Remove fixed dimensions
    
    * Add some CSS variables
    
    * Convert Tooltip styles to SCSS
    
    * Center tooltip in storybook
    
    * Add visibility animation in Tooltip
    
    * Add Tooltip placements
    
    * Import global colors and variables in Tooltip

---
+++ src/components/tooltip/index.js
@@ -0,0 +1,11 @@
+import { h } from 'preact';
+import styles from './styles';
+import { createClassName } from '../helpers';
+
+const Tooltip = ({ children, hidden = false, placement, ...props }) => (
+	<div className={createClassName(styles, 'tooltip', { hidden, placement })} {...props}>
+		{children}
+	</div>
+);
+
+export default Tooltip;
