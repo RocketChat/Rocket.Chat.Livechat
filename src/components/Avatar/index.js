7c4c3e04bb0105e0f4b15e358bb86317a7d2499a
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-09-13 15:08:46 -0300

    Add knobs in Avatar stories

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -3,8 +3,6 @@ import styles from './styles';
 import { createClassName } from '../helpers';
 import placeholder from './placeholder.png';
 
-const getStyles = (styles, name, classes) => [styles[name], ...Object.entries(styles).filter(([key]) => classes[key]).map(([, value]) => value)].join(' ');
-
 const Avatar = ({ small, large, src, description, ...args }) => (
 	<div
 		aria-label="User picture"
