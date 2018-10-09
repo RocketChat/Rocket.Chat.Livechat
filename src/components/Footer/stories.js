97c45ace3e7fecebce1ebdabf68912bfb47ba800
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-10-09 18:32:29 -0300

    Update Footer styles

--- src/components/Footer/stories.js
+++ src/components/Footer/stories.js
@@ -2,11 +2,23 @@ import { h } from 'preact';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import Footer, { Container, Powered } from '.';
+import Footer, { Container, PoweredBy } from '.';
 import Composer from '../Composer';
 
 const stickyFooterDecorator = (storyFn) => (
-	<div style={{ position: 'fixed', top: 0, left: 0, bottom: 0, right: 0, display: 'flex', flexDirection: 'column', justifyContent: 'stretch', overflow: 'auto' }}>
+	<div
+		style={{
+			position: 'fixed',
+			top: 0,
+			left: 0,
+			bottom: 0,
+			right: 0,
+			display: 'flex',
+			flexDirection: 'column',
+			justifyContent: 'stretch',
+			overflow: 'auto',
+		}}
+	>
 		<div style={{ flex: '1' }} />
 		{storyFn()}
 	</div>
@@ -17,7 +29,7 @@ storiesOf('Components|Footer', module)
 	.add('with PoweredBy', () => (
 		<Footer onClick={action('clicked')}>
 			<Container>
-				<Powered />
+				<PoweredBy />
 			</Container>
 		</Footer>
 	))
@@ -27,7 +39,7 @@ storiesOf('Components|Footer', module)
 				<Composer placeholder="Insert your text here" />
 			</Container>
 			<Container>
-				<Powered />
+				<PoweredBy />
 			</Container>
 		</Footer>
 	));
