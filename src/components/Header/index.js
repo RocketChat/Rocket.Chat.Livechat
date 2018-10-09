77b9dc074f0e2ba81fbe0b320dc0e269880cd5e2
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-10-09 19:48:47 -0300

    Update Header markup and styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,47 @@
 import { h } from 'preact';
 import styles from './styles';
+import { createClassName } from '../helpers';
 
-const Header = ({ children, color, ...args }) => (
-	<header {...args} class={styles.header} style={`background-color:${ color }`}>
+export const Header = ({ children, color, ...props }) => (
+	<header className={createClassName(styles, 'header')} style={{ backgroundColor: color }} {...props}>
 		{children}
 	</header>
 );
 
-export const Avatar = ({ children }) => <div class={styles.avatar}>{children}</div>;
-export const Actions = ({ children }) => <nav class={styles.actions}>{children}</nav>;
-export const Action = (({ children, ...args }) => (<button {...args} class={styles.action}>{children}</button>));
-export const Content = ({ children }) => <div class={styles.content}>{children}</div>;
-export const Title = ({ children }) => <div class={styles.title}>{children}</div>;
-export const SubTitle = ({ children }) => <div class={styles.subTitle}>{children}</div>;
-Header.Avatar = Avatar;
-Header.Actions = Actions;
-Header.Action = Action;
-Header.Content = Content;
-Header.Title = Title;
-Header.SubTitle = Actions;
+export const Picture = ({ children, ...props }) => (
+	<div className={createClassName(styles, 'header__picture')} {...props}>
+		{children}
+	</div>
+);
+
+export const Content = ({ children, ...props }) => (
+	<div className={createClassName(styles, 'header__content')} {...props}>
+		{children}
+	</div>
+);
+
+export const Title = ({ children, ...props }) => (
+	<div className={createClassName(styles, 'header__title')} {...props}>
+		{children}
+	</div>
+);
+
+export const SubTitle = ({ children, ...props }) => (
+	<div className={createClassName(styles, 'header__subtitle')} {...props}>
+		{children}
+	</div>
+);
+
+export const Actions = ({ children, ...props }) => (
+	<nav className={createClassName(styles, 'header__actions')} {...props}>
+		{children}
+	</nav>
+);
+
+export const Action = ({ children, ...props }) => (
+	<button className={createClassName(styles, 'header__action')} {...props}>
+		{children}
+	</button>
+);
+
 export default Header;
--- src/components/Header/stories.js
+++ src/components/Header/stories.js
@@ -1,33 +1,30 @@
 import { h } from 'preact';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { color, withKnobs } from '@storybook/addon-knobs';
-import Header from '.';
+import { withKnobs, color, text } from '@storybook/addon-knobs';
+
+import Header, { Picture, Content, SubTitle, Title } from '.';
 import Avatar from '../Avatar';
 import bertieBartonAvatar from '../Avatar/bertieBarton.png';
-import centered from '@storybook/addon-centered';
 
 
 storiesOf('Components|Header', module)
-	.addDecorator(centered)
 	.addDecorator(withKnobs)
-	.add('with text content', () => <Header onClick={action('clicked')}>Need Help?</Header>)
-
+	.add('with text content', () => (
+		<Header color={color('color', '#175CC4')} onClick={action('clicked')}>
+			Need Help?
+		</Header>
+	))
 	.add('with agent', () => (
-		<Header onClick={action('clicked')}>
-			<Header.Avatar><Avatar src={bertieBartonAvatar} /></Header.Avatar>
-			<Header.Content>
-				<Header.Title>@bertie.barton</Header.Title>
-				<Header.SubTitle>Available</Header.SubTitle>
-			</Header.Content>
+		<Header color={color('color', '#175CC4')} onClick={action('clicked')}>
+			<Picture>
+				<Avatar src={bertieBartonAvatar} />
+			</Picture>
+
+			<Content>
+				<Title>{text('title', '@bertie.barton')}</Title>
+				<SubTitle>{text('subtitle', 'Available')}</SubTitle>
+			</Content>
 		</Header>)
 	)
-	.add('custom color', () => (
-		<Header color={color('color', '#175CC4')} onClick={action('clicked')}>
-			<Header.Avatar><Avatar src={bertieBartonAvatar} /></Header.Avatar>
-			<Header.Content>
-				<Header.Title>@bertie.barton</Header.Title>
-				<Header.SubTitle>Available</Header.SubTitle>
-			</Header.Content>
-		</Header>)
-	);
+;
