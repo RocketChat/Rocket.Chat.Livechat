2216f9e03a6d7f74a790fd45208f4bc08b0f70b4
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-10-10 15:20:50 -0300

    More stories for Header

--- src/components/Header/stories.js
+++ src/components/Header/stories.js
@@ -1,7 +1,7 @@
 import { h } from 'preact';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, color, text } from '@storybook/addon-knobs';
+import { withKnobs, color, select, text } from '@storybook/addon-knobs';
 
 import Header, { Picture, Content, SubTitle, Title, Actions, Action } from '.';
 import Avatar from '../Avatar';
@@ -9,7 +9,7 @@ import StatusIndicator, { statuses } from '../StatusIndicator';
 import Bell from 'icons/bell.svg';
 import Arrow from 'icons/arrow.svg';
 import NewWindow from 'icons/newWindow.svg';
-import { select } from '@storybook/addon-knobs/dist/vue';
+import bertieBartonAvatar from '../Avatar/bertieBarton.png';
 
 
 storiesOf('Components|Header', module)
@@ -17,29 +17,49 @@ storiesOf('Components|Header', module)
 	.add('with text content', () => (
 		<Header color={color('color', '#175CC4')} onClick={action('clicked')}>
 			<Content>
-				Need Help?
+				{text('text', 'Need Help?')}
+			</Content>
+		</Header>
+	))
+	.add('with title and subtitle', () => (
+		<Header color={color('color', '#175CC4')} onClick={action('clicked')}>
+			<Content>
+				<Title>{text('title', 'Rocket.Chat')}</Title>
+				<SubTitle>{text('subtitle', 'Livechat')}</SubTitle>
 			</Content>
 		</Header>
 	))
 	.add('with picture', () => (
 		<Header color={color('color', '#175CC4')} onClick={action('clicked')}>
 			<Picture>
-				<Avatar />
+				<Bell width={20} />
 			</Picture>
 
 			<Content>
-				<Title>{text('title', '@bertie.barton')}</Title>
-				<SubTitle>
-					<StatusIndicator status={select('status', statuses, 'online')} />
-					<span style={{ margin: '5px' }}>{text('subtitle', 'Available')}</span>
-				</SubTitle>
+				{text('text', 'Notification settings')}
 			</Content>
-		</Header>)
-	)
+		</Header>
+	))
 	.add('with actions', () => (
+		<Header color={color('color', '#175CC4')} onClick={action('clicked')}>
+			<Content>
+				{text('text', 'Chat finished')}
+			</Content>
+
+			<Actions>
+				<Action onClick={action('notifications')}>
+					<Bell width={20} />
+				</Action>
+				<Action onClick={action('minimize')}>
+					<Arrow width={20} />
+				</Action>
+			</Actions>
+		</Header>
+	))
+	.add('for user chat', () => (
 		<Header color={color('color', '#175CC4')}>
 			<Picture>
-				<Avatar />
+				<Avatar src={bertieBartonAvatar} />
 			</Picture>
 
 			<Content>
