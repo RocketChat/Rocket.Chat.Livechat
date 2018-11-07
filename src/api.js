d99cc2c837d82808937c320e09dd3772f175dde7
Guilherme Gazzo <guilhermegazzo@gmail.com>
2018-11-07 17:19:59 -0200

    chunks

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
-import Livechat from 'PUT PATH HERE/Rocket.Chat.js.SDK/dist/lib/clients/Livechat'; // TODO CHANGE
-const livechat = new Livechat({ host: 'http://localhost:3000' });
+import Livechat from '/Users/guilhermegazzo/Rocket.Chat.js.SDK/dist/lib/clients/Livechat';
+const livechat = new Livechat({ host: 'http://localhost:3000', protocol: 'ddp' });
 export default livechat;
