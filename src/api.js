135eb9626103e92e44160266dca64bded4ff80fe
Renato Becker <renato.augusto.becker@gmail.com>
2018-11-08 11:57:30 -0200

    Improvements on Livechat.

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
 import Livechat from '/Volumes/Workspace/Rocket.Chat.js.SDK/dist/lib/clients/Livechat'; // TODO CHANGE
-const livechat = new Livechat({ host: 'http://192.168.1.218:3000', 'protocol': 'ddp' });
+const livechat = new Livechat({ host: 'localhost:3000', 'protocol': 'ddp' });
 export default livechat;
