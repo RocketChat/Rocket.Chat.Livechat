81d3a7a0a053cd0a1ea8673ddc47e3315194ca73
Tasso Evangelista <tasso@tassoevan.me>
2018-11-14 16:34:41 -0200

    [IMPROVE] Composer paste and drop (#63)
    * Refactor Composer
    
    * Add onChange to Composer
    
    * Set text pasting in Composer
    
    * Set dropping in Composer
    
    * Update Composer stories
    
    * Add missing onUpload event to Composer stories

--- src/components/Composer/index.js
+++ src/components/Composer/index.js
@@ -1,71 +1,133 @@
 import { h, Component } from 'preact';
 import { createClassName } from '../helpers';
+import { parse } from './parsing';
 import styles from './styles';
 
 
-export const Action = ({ children, onClick, ...args }) =>
-	(
-		<button
-			onClick={(e) => {
-				e.preventDefault();
-				return onClick && onClick(e);
-			}}
-			{...args}
-			class={styles.action}
-		>{children}</button>
-	);
+const handleActionClick = (onClick) => (event) => {
+	event.preventDefault();
+	onClick && onClick(event);
+};
 
-export const Actions = ({ children }) => <div class={styles.actions}>{children}</div>;
+export const Actions = ({ children, ...args }) => (
+	<div className={createClassName(styles, 'composer__actions')} {...args}>{children}</div>
+);
 
-export default class Composer extends Component {
-	bind(el) {
+export const Action = ({ children, onClick, ...args }) => (
+	<button
+		onClick={handleActionClick(onClick)}
+		className={createClassName(styles, 'composer__action')}
+		{...args}
+	>{children}</button>
+);
+
+export class Composer extends Component {
+	bind = (el) => {
 		this.el = el;
 	}
+
 	get value() {
 		return this.el.innerText;
 	}
-	onKeypress(event) {
-		const { which, shiftKey } = event;
-		if (which === 13 && !shiftKey) {
-			this.props.onSubmit && this.props.onSubmit(this.el.innerText);
-			this.el.innerText = '';
+
+	handleInput = (onChange) => () => {
+		onChange && onChange(this.el.innerText);
+	}
+
+	handleKeypress = (onSubmit) => (event) => {
+		if (event.which === 13 && !event.shiftKey) {
 			event.preventDefault();
+			onSubmit && onSubmit(this.el.innerText);
+			this.el.innerText = '';
 		}
 	}
-	onPaste(e) {
-		if (e.clipboardData == null || !e.clipboardData.items) {
+
+	handlePaste = (onUpload) => async(event) => {
+		if (!event.clipboardData || !event.clipboardData.items) {
 			return;
 		}
-		const items = Array.from(e.clipboardData.items);
-		const files = items
-			.filter((item) => (item.kind === 'file' && item.type.indexOf('image/') !== -1))
-			.map((item) => item.getAsFile());
 
-		this.props.onUpload(files);
-		e.preventDefault();
+		event.preventDefault();
+
+		const items = Array.from(event.clipboardData.items);
+
+		const files = items.filter((item) => (item.kind === 'file' && /^image\//.test(item.type)))
+			.map((item) => item.getAsFile());
+		if (files.length) {
+			onUpload && onUpload(files);
+			return;
+		}
+
+		const texts = await Promise.all(
+			items.filter((item) => (item.kind === 'string' && /^text\/plain/.test(item.type)))
+				.map((item) => new Promise((resolve) => item.getAsString(resolve)))
+		);
+		texts.forEach((text) => this.pasteText(text));
 	}
-	input(/* event*/) {
-		// const { inputType, data } = event;
-		// if (inputType === 'insertParagraph' || (inputType === 'insertText' && data === null)) {
-		// 	this.props.onSubmit && this.props.onSubmit(this.el.innerText);
-		// 	this.el.innerText = '';
-		// }
+
+	handleDrop = (onUpload) => async(event) => {
+		if (!event.dataTransfer || !event.dataTransfer.items) {
+			return;
+		}
+
+		event.preventDefault();
+
+		const items = Array.from(event.dataTransfer.items);
+
+		const files = items.filter((item) => (item.kind === 'file' && /^image\//.test(item.type)))
+			.map((item) => item.getAsFile());
+		if (files.length) {
+			onUpload && onUpload(files);
+			return;
+		}
+
+		const texts = await Promise.all(
+			items.filter((item) => (item.kind === 'string' && /^text\/plain/.test(item.type)))
+				.map((item) => new Promise((resolve) => item.getAsString(resolve)))
+		);
+		texts.forEach((text) => this.pasteText(text));
 	}
-	constructor() {
-		super();
-		this.bind = this.bind.bind(this);
-		this.input = this.input.bind(this);
-		this.onKeypress = this.onKeypress.bind(this);
-		this.onPaste = this.onPaste.bind(this);
+
+	pasteText = (plainText) => {
+		this.el.focus();
+
+		if (document.queryCommandSupported('insertText')) {
+			document.execCommand('insertText', false, plainText);
+			return;
+		}
+
+		const range = document.getSelection().getRangeAt(0);
+		range.deleteContents();
+		const textNode = document.createTextNode(plainText);
+		range.insertNode(textNode);
+		range.selectNodeContents(textNode);
+		range.collapse(false);
+
+		const selection = window.getSelection();
+		selection.removeAllRanges();
+		selection.addRange(range);
 	}
-	render({ pre, post, placeholder, ...args }) {
+
+	render({ pre, post, placeholder, value, onChange, onSubmit, onUpload, ...args }) {
 		return (
-			<div {...args} className={createClassName(styles, 'composer', {})}>
+			<div className={createClassName(styles, 'composer')} {...args}>
 				{pre}
-				<div ref={this.bind} onPaste={this.onPaste} onKeypress={this.onKeypress} onInput={this.input} placeholder={placeholder} className={createClassName(styles, 'composer__input', {})}
-					contenteditable
+				<div
+					ref={this.bind}
+					// eslint-disable-next-line react/no-danger
+					dangerouslySetInnerHTML={{ __html: parse(value) }}
+					data-placeholder={placeholder}
+					onInput={this.handleInput(onChange)}
+					onKeypress={this.handleKeypress(onSubmit)}
+					onPaste={this.handlePaste(onUpload)}
+					onDrop={this.handleDrop(onUpload)}
+					className={createClassName(styles, 'composer__input')}
+					contentEditable
 				/>
 				{post}
-			</div>);
-	}
+			</div>
+		);
+	 }
 }
+
+export default Composer;
