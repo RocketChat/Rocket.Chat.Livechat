23e71877ef70a9e0a849208a908c8dad8b1aed92
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-11-21 12:32:23 -0200

    Add storybook console addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,17 @@
 import { configure } from '@storybook/react';
+import { setConsoleOptions } from '@storybook/addon-console';
 import { setOptions } from '@storybook/addon-options';
+
 
 setOptions({
   name: 'RocketChat Livechat',
 	url: 'https://github.com/RocketChat/Rocket.Chat.Livechat',
   hierarchySeparator: /\//,
   hierarchyRootSeparator: /\|/,
+});
+
+setConsoleOptions({
+	panelExclude: [],
 });
 
 function loadStories() {
