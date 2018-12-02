77e961489c63920421bc434cf86ef53c1a8ecd16
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-12-02 00:10:23 -0200

    Move store state to Register props mapping to RegisterConnector

--- src/routes/Register/container.js
+++ src/routes/Register/container.js
@@ -9,46 +9,54 @@ export class RegisterContainer extends Component {
 		const { dispatch, token } = this.props;
 
 		await dispatch({ loading: true });
-		const user = await SDK.grantVisitor({ visitor: { ...fields, token } });
-		await dispatch({ loading: false });
-		await dispatch({ token: user.token, user });
+		try {
+			const user = await SDK.grantVisitor({ visitor: { ...fields, token } });
+			await dispatch({ token: user.token, user });
+		} finally {
+			await dispatch({ loading: false });
+		}
 	}
 
-	render = ({
-		ref,
-		loading = false,
-		theme: {
-			title,
-			color,
-		} = {},
-		strings: {
-			registrationFormMessage: message,
-		} = {},
-		settings: {
-			nameFieldRegistrationForm: hasNameField,
-			emailFieldRegistrationForm: hasEmailField,
-			allowSwitchingDepartments: hasDepartmentField,
-		} = {},
-	}) => (
-		<Register
-			ref={ref}
-			title={title || I18n.t('Need help?')}
-			color={color}
-			message={message || I18n.t('Please, tell us some informations to start the chat')}
-			hasNameField={hasNameField}
-			hasEmailField={hasEmailField}
-			hasDepartmentField={hasDepartmentField}
-			departments={[]}
-			loading={loading}
-			onSubmit={this.handleSubmit}
-		/>
+	render = (props) => (
+		<Register {...props} onSubmit={this.handleSubmit} />
 	)
 }
 
 
 export const RegisterConnector = ({ ref, ...props }) => (
 	<Consumer>
-		{(state) => <RegisterContainer ref={ref} {...props} {...state} />}
+		{({
+			theme: {
+				title,
+				color,
+			} = {},
+			strings: {
+				registrationFormMessage: message,
+			} = {},
+			settings: {
+				nameFieldRegistrationForm: hasNameField,
+				emailFieldRegistrationForm: hasEmailField,
+				allowSwitchingDepartments: hasDepartmentField,
+			} = {},
+			loading = false,
+			token,
+			dispatch,
+		}) => (
+			<RegisterContainer
+				ref={ref}
+				{...props}
+				title={title || I18n.t('Need help?')}
+				color={color}
+				message={message || I18n.t('Please, tell us some informations to start the chat')}
+				hasNameField={hasNameField}
+				hasEmailField={hasEmailField}
+				hasDepartmentField={hasDepartmentField}
+				departments={[]}
+				loading={loading}
+				token={token}
+				dispatch={dispatch}
+			/>
+		)}
 	</Consumer>
 );
 
