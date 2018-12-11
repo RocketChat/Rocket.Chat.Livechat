1150e0575d7ac2dd7da4625e467fb941f6851d06
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-12-11 15:34:54 -0200

    Support className in Avatar

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -5,7 +5,7 @@ import { createClassName } from '../helpers';
 const Avatar = ({ small, large, src, description, className, ...args }) => (
 	<div
 		aria-label="User picture"
-		className={createClassName(styles, 'avatar', { small, large, nobg: src }, className)}
+		className={createClassName(styles, 'avatar', { small, large, nobg: src }, [className])}
 		{...args}
 	>
 		{src && <img alt={description} className={createClassName(styles, 'avatar__image')} src={src} />}
