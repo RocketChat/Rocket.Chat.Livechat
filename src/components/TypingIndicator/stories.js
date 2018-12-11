36679e262775dd1a394c3b214a4c0905bb21cca2
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-12-11 15:41:34 -0200

    Update TypingIndicator

--- src/components/TypingIndicator/stories.js
+++ src/components/TypingIndicator/stories.js
@@ -1,25 +1,34 @@
-import { h } from 'preact';
-import centered from '@storybook/addon-centered';
 import { storiesOf } from '@storybook/react';
+import centered from '@storybook/addon-centered';
+import TypingIndicator, { TypingDots } from '.';
 
-import Typing, { TypingAvatar, TypingIndicator } from '.';
-
-import bertieBartonAvatar from '../Avatar/bertieBarton.png';
-import avatar1 from './avatar1.png';
-import avatar2 from './avatar2.png';
-import avatar3 from './avatar3.png';
 
 storiesOf('Components|TypingIndicator', module)
 	.addDecorator(centered)
-	.add('three dots', () => <TypingIndicator>The attendant is typing</TypingIndicator>)
-	.add('multiple Avatars', () => (
-		<TypingAvatar avatars={[
-			bertieBartonAvatar,
-			avatar1,
-			avatar2,
-			avatar3,
-		]}
-		/>))
+	.add('only dots', () => (
+		<TypingDots>{I18n.t('The attendant is typing')}</TypingDots>
+	))
 	.add('as message', () => (
-		<Typing users={[bertieBartonAvatar, avatar1, avatar2, avatar3]} description={I18n.t('The attendant is typing')} />
-	));
+		<TypingIndicator>{I18n.t('The attendant is typing')}</TypingIndicator>
+	))
+	.add('with avatars', () => (
+		<TypingIndicator
+			avatars={[
+				{
+					description: 'guilherme.gazzo',
+					src: '//gravatar.com/avatar/7ba3fcdd590033117b1e6587e0d20478?s=32',
+				},
+				{
+					description: 'tasso.evangelista',
+					src: '//gravatar.com/avatar/5ddcdc159b17f4f79fd254a06d871c5a?s=32',
+				},
+				{
+					description: 'martin.schoeler',
+					src: '//gravatar.com/avatar/e6662ba16ba3ca2a76857e3999e6d960?s=32',
+				},
+			]}
+		>
+			{I18n.t('The attendant is typing')}
+		</TypingIndicator>
+	))
+;
