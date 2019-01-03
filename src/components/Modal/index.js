8e2300f02bc81126eba0897cdccda118a94632cf
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-01-03 19:09:15 -0200

    Update Modal component

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,15 +1,13 @@
-import { h } from 'preact';
 import styles from './styles';
 import { createClassName } from '../helpers';
 
-const Modal = ({ open, children, overlay = true, animated, ...args }) => (
-	(open && <div
 
-		className={createClassName(styles, 'modal', { overlay })}
-		{...args}
-	         >
-		<div className={createClassName(styles, 'container', { animated })} {...args}>{children}</div>
-	</div>)
+export const Modal = ({ children, animated, open, ...props }) => (
+	open ?
+		<div className={createClassName(styles, 'modal__overlay')}>
+			<div className={createClassName(styles, 'modal', { animated })} {...props}>{children}</div>
+		</div> :
+		null
 );
 
 export default Modal;
--- src/components/Modal/stories.js
+++ src/components/Modal/stories.js
@@ -1,21 +1,46 @@
-import { h } from 'preact';
+import loremIpsum from 'lorem-ipsum';
+import mem from 'mem';
+import { storiesOf } from '@storybook/react';
 import centered from '@storybook/addon-centered';
 import { withKnobs, boolean, text } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react';
-
 import Modal from '.';
+
+
+const memedIpsum = mem(loremIpsum);
+
+
+const LoremIpsum = ({ padding = '5rem', count = 5, units = 'paragraphs', ...options }) => (
+	<div style={{ padding }}>
+		{memedIpsum({ count, units, ...options }).split('\n').map((paragraph) => <p>{paragraph}</p>)}
+	</div>
+);
+
 
 storiesOf('Components|Modal', module)
 	.addDecorator(centered)
 	.addDecorator(withKnobs)
 	.add('opened', () => (
 		<div>
-			Hello Modal!
-			<Modal open={boolean('open', true)} overlay={boolean('overlay', true)}>{text('text', 'hey')}</Modal>
+			<LoremIpsum />
+			<Modal open={boolean('open', true)} animated={boolean('animated', false)}>
+				{text('content', memedIpsum({ count: 1, units: 'paragraphs' }))}
+			</Modal>
 		</div>
-	)).add('closed', () => (
+	))
+	.add('closed', () => (
 		<div>
-			Hello Modal!
-			<Modal open={boolean('open', false)} overlay={boolean('overlay', true)}>{text('text', 'hey')}</Modal>
+			<LoremIpsum />
+			<Modal open={boolean('open', false)} animated={boolean('animated', false)}>
+				{text('content', memedIpsum({ count: 1, units: 'paragraphs' }))}
+			</Modal>
 		</div>
-	));
+	))
+	.add('animated', () => (
+		<div>
+			<LoremIpsum />
+			<Modal open={boolean('open', true)} animated={boolean('animated', true)}>
+				{text('content', memedIpsum({ count: 1, units: 'paragraphs' }))}
+			</Modal>
+		</div>
+	))
+;
