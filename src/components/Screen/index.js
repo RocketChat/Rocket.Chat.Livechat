726d896553a296e9f0ed7fe179ea386c9e2756bc
diegolmello <diegolmello@gmail.com>
2019-01-11 15:44:13 -0200

    Add Alert component

--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -1,4 +1,5 @@
 import { Component } from 'preact';
+import Alert from '../Alert';
 import Avatar from '../Avatar';
 import Header from '../Header';
 import Footer from '../Footer';
@@ -40,6 +41,10 @@ export class Screen extends Component {
 		onOpenWindow && onOpenWindow();
 	}
 
+	handleRef = (ref) => {
+		this.headerRef = ref;
+	}
+
 	render = ({
 		color,
 		agent,
@@ -53,10 +58,20 @@ export class Screen extends Component {
 		options,
 		onChangeDepartment,
 		onFinishChat,
+		onDismissAlert,
 		className,
+		alerts,
 	}) => (
 		<div className={createClassName(styles, 'screen', { rounded: !windowed }, [className])}>
-			<Header color={color}>
+			<Header
+				ref={this.handleRef}
+				color={color}
+				post={
+					<Header.Post headerRef={this.headerRef}>
+						{alerts && alerts.map((alert) => <Alert {...alert} onDismiss={onDismissAlert}>{alert.children}</Alert>)}
+					</Header.Post>
+				}
+			>
 				{agent && agent.avatar && (
 					<Header.Picture>
 						<Avatar src={agent.avatar.src} description={agent.avatar.description} />
@@ -138,6 +153,11 @@ export class ScreenContainer extends Component {
 		dispatch({ sound: { ...sound, enabled: false } });
 	}
 
+	handleDismissAlert = (id) => {
+		const { dispatch, alerts = [] } = this.props;
+		dispatch({ alerts: alerts.filter((alert) => alert.id !== id) });
+	}
+
 	render = (props) => (
 		<Screen
 			{...props}
@@ -146,6 +166,7 @@ export class ScreenContainer extends Component {
 			onMinimize={this.handleMinimize}
 			onRestore={this.handleRestore}
 			onOpenWindow={this.handleOpenWindow}
+			onDismissAlert={this.handleDismissAlert}
 		/>
 	)
 }
@@ -155,6 +176,7 @@ export const ScreenConnector = ({ ref, ...props }) => (
 	<Consumer>
 		{({
 			sound = {},
+			alerts = [],
 			dispatch = () => {},
 		} = {}) => (
 			<ScreenContainer
@@ -164,6 +186,7 @@ export const ScreenConnector = ({ ref, ...props }) => (
 				minimized={false}
 				windowed={false}
 				sound={sound}
+				alerts={alerts}
 				dispatch={dispatch}
 			/>
 		)}
