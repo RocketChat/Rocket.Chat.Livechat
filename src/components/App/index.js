51e4c65c5b766519239284796328adb27b215905
Tasso Evangelista <tasso@tassoevan.me>
2019-01-16 15:01:59 -0200

    [WIP] Change Screen state handling (#101)
    * Make Screen stateless
    
    * Injecting minimized state into Screen

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -12,6 +12,7 @@ import CustomFields from '../../lib/customFields';
 import Triggers from '../../lib/triggers';
 import userPresence from '../../lib/userPresence';
 
+
 export class App extends Component {
 
 	state = { initialized: false }
@@ -27,6 +28,46 @@ export class App extends Component {
 			Triggers.init();
 		}
 	}
+
+	handleEnableNotifications = () => {
+		const { dispatch, sound = {} } = this.props;
+		dispatch({ sound: { ...sound, enabled: true } });
+	}
+
+	handleDisableNotifications = () => {
+		const { dispatch, sound = {} } = this.props;
+		dispatch({ sound: { ...sound, enabled: false } });
+	}
+
+	handleMinimize = () => {
+		const { dispatch } = this.props;
+		dispatch({ minimized: true });
+	}
+
+	handleRestore = () => {
+		const { dispatch } = this.props;
+		dispatch({ minimized: false });
+	}
+
+	handleDismissAlert = (id) => {
+		const { dispatch, alerts = [] } = this.props;
+		dispatch({ alerts: alerts.filter((alert) => alert.id !== id) });
+	}
+
+	getScreenProps = () => ({
+		notificationsEnabled: this.props.sound && this.props.sound.enabled,
+		minimized: this.props.minimized,
+		windowed: this.props.windowed,
+		sound: this.props.sound,
+		alerts: this.props.alerts,
+		modal: this.props.modal,
+		onEnableNotifications: this.handleEnableNotifications,
+		onDisableNotifications: this.handleDisableNotifications,
+		onMinimize: this.handleMinimize,
+		onRestore: this.handleRestore,
+		onOpenWindow: this.handleOpenWindow,
+		onDismissAlert: this.handleDismissAlert,
+	})
 
 	async initialize() {
 		await loadConfig();
@@ -50,45 +91,44 @@ export class App extends Component {
 		this.finalize();
 	}
 
-	renderScreen() {
-		const {
-			config: {
-				settings: {
-					registrationForm,
-					nameFieldRegistrationForm,
-					emailFieldRegistrationForm,
-					forceAcceptDataProcessingConsent: gdprRequired,
-				},
-				online,
+	renderScreen = ({
+		config: {
+			settings: {
+				registrationForm,
+				nameFieldRegistrationForm,
+				emailFieldRegistrationForm,
+				forceAcceptDataProcessingConsent: gdprRequired,
 			},
-			gdpr: {
-				accepted: gdprAccepted,
-			},
-			triggered,
-			user,
-		} = this.props;
-
+			online,
+		},
+		gdpr: {
+			accepted: gdprAccepted,
+		},
+		triggered,
+		user,
+	}) => {
 		// Temporary implementation, the best approach for this resource is handling the the Router component
 		if (gdprRequired && !gdprAccepted) {
-			return <GDPRAgreement default path="/gdpr" />;
+			return <GDPRAgreement default path="/gdpr" {...this.getScreenProps()} />;
 		}
 
 		if (!online) {
-			return <LeaveMessage default path="/LeaveMessage" />;
+			return <LeaveMessage default path="/LeaveMessage" {...this.getScreenProps()} />;
 		}
 
 		const showRegistrationForm = registrationForm && (nameFieldRegistrationForm || emailFieldRegistrationForm);
 		if ((user && user.token) || !showRegistrationForm || triggered) {
-			return <Chat default path="/home" />;
+			return <Chat default path="/home" {...this.getScreenProps()} />;
 		}
-		return <Register default path="/register" />;
+
+		return <Register default path="/register" {...this.getScreenProps()} />;
 	}
 
-	render = () => (this.state.initialized &&
+	render = (props, { initialized }) => initialized && (
 		<Router onChange={this.handleRoute}>
-			{this.renderScreen()}
-			<ChatFinished path="/chat-finished" />
-			<SwitchDepartment path="/switch-department" />
+			{this.renderScreen(props)}
+			<ChatFinished path="/chat-finished" {...this.getScreenProps()} />
+			<SwitchDepartment path="/switch-department" {...this.getScreenProps()} />
 		</Router>
 	)
 }
@@ -102,12 +142,24 @@ const AppConnector = () => (
 					user,
 					triggered,
 					gdpr,
+					sound,
+					minimized,
+					windowed,
+					alerts,
+					modal,
+					dispatch,
 				}) => (
 					<App
 						config={config}
 						gdpr={gdpr}
 						triggered={triggered}
 						user={user}
+						sound={sound}
+						minimized={minimized}
+						windowed={windowed}
+						alerts={alerts}
+						modal={modal}
+						dispatch={dispatch}
 					/>
 				)}
 			</StoreConsumer>
