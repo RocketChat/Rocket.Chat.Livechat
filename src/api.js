89376e583d75d78328d05a0819d9ffc5ae1ce2af
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-01-24 17:51:10 -0200

    Get server URL from __meteor_runtime_config__ too

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import queryString from 'query-string';
 import LivechatClient from '@rocket.chat/sdk/lib/clients/Livechat';
 
+const host = (
+	(window.__meteor_runtime_config__ && window.__meteor_runtime_config__.ROOT_URL) ||
+	queryString.parse(window.location.search).serverUrl ||
+	(process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : null)
+);
 
-const {
-	serverUrl = process.env.NODE_ENV === 'development' && 'http://localhost:3000',
-} = queryString.parse(window.location.search);
-
-export const Livechat = new LivechatClient({ host: serverUrl, protocol: 'ddp' });
+export const Livechat = new LivechatClient({ host, protocol: 'ddp' });
