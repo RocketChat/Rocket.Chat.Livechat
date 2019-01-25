1fa395d46302b4cd1b66e15aee3b9e3676145fb2
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-01-25 11:30:38 -0200

    Remove Meteor-related references

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import queryString from 'query-string';
 import LivechatClient from '@rocket.chat/sdk/lib/clients/Livechat';
 
 const host = (
-	(window.__meteor_runtime_config__ && window.__meteor_runtime_config__.ROOT_URL) ||
+	window.SERVER_URL ||
 	queryString.parse(window.location.search).serverUrl ||
 	(process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : null)
 );
