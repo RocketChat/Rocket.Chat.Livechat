d89edbeaac24e8556c045d90c6fad31261244c12
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-03-18 18:16:18 -0300

    Embed ChatButton into Screen component

--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -1,7 +1,7 @@
 import { Component } from 'preact';
 import { Alert } from '../Alert';
 import { Avatar } from '../Avatar';
-import ChatButton from '../ChatButton';
+import { Button } from '../Button';
 import Header from '../Header';
 import Footer from '../Footer';
 import { PopoverContainer } from '../Popover';
@@ -13,6 +13,8 @@ import NotificationsDisabledIcon from '../../icons/bellOff.svg';
 import MinimizeIcon from '../../icons/arrowDown.svg';
 import RestoreIcon from '../../icons/arrowUp.svg';
 import OpenWindowIcon from '../../icons/newWindow.svg';
+import ChatIcon from '../../icons/chat.svg';
+import CloseIcon from '../../icons/close.svg';
 import styles from './styles';
 
 
@@ -128,6 +130,21 @@ export const ScreenFooter = ({ children, options }) => (
 	</Footer>
 );
 
+const ChatButton = ({
+	text,
+	minimized,
+	badge,
+	onClick,
+}) => (
+	<Button
+		icon={minimized ? <ChatIcon /> : <CloseIcon />}
+		badge={badge}
+		onClick={onClick}
+		className={createClassName(styles, 'screen__chat-button')}
+	>
+		{text}
+	</Button>
+);
 
 export const Screen = ({
 	theme = {},
@@ -185,10 +202,9 @@ export const Screen = ({
 
 		<ChatButton
 			text={title}
-			open={!minimized}
-			onClick={minimized ? onRestore : onMinimize}
-			className={createClassName(styles, 'screen__chat-button')}
 			badge={unread}
+			minimized={minimized}
+			onClick={minimized ? onRestore : onMinimize}
 		/>
 
 		{sound && <Sound src={sound.src} play={sound.play} onStop={onSoundStop} />}
