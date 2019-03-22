37569d866bef7d5341d721cd85e9ea8b4e0c9f0a
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-03-22 18:49:50 -0300

    Remove Storybook's console addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,5 @@
 import { addDecorator, configure, addParameters } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
-import { setConsoleOptions } from '@storybook/addon-console';
 import { withOptions } from '@storybook/addon-options';
 import { create } from '@storybook/theming';
 
@@ -26,10 +25,6 @@ addParameters({
 });
 
 addDecorator(withA11y);
-
-setConsoleOptions({
-	panelExclude: [],
-});
 
 function loadStories() {
 	require('../src/styles/index.scss');
