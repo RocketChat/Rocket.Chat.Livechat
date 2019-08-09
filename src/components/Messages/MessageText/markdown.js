5984b8b5d4059b990e50eb298c7edc66ac7d0898
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-08-09 10:44:08 -0300

    Make message markdown links open externally

--- src/components/Messages/MessageText/markdown.js
+++ src/components/Messages/MessageText/markdown.js
@@ -6,6 +6,27 @@ const md = new MarkdownIt({
 	typographer: true,
 });
 
+const defaultRender = md.renderer.rules.link_open || ((tokens, idx, options, env, self) => self.renderToken(tokens, idx, options));
+
+md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
+	const targetAttrIndex = tokens[idx].attrIndex('target');
+	const relAttrIndex = tokens[idx].attrIndex('rel');
+
+	if (targetAttrIndex < 0) {
+		tokens[idx].attrPush(['target', '_blank']);
+	} else {
+		tokens[idx].attrs[targetAttrIndex][1] = '_blank';
+	}
+
+	if (relAttrIndex < 0) {
+		tokens[idx].attrPush(['rel', 'noopener noreferrer']);
+	} else {
+		tokens[idx].attrs[relAttrIndex][1] = 'noopener noreferrer';
+	}
+
+	return defaultRender(tokens, idx, options, env, self);
+};
+
 md.use((md) => {
 	const renderStrong = (tokens, idx, opts, _, slf) => {
 		const token = tokens[idx];
