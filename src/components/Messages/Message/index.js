d2d1ef68295c4d923681ba5ebd88c54d23a7e5d7
Ashwani Yadav <ashyadavash@gmail.com>
2020-03-10 03:37:38 +0530

    [FIX] Livechat guest avatar using name instead of username

--- src/components/Messages/Message/index.js
+++ src/components/Messages/Message/index.js
@@ -88,7 +88,8 @@ export const Message = memo(({
 	>
 		<MessageAvatars
 			avatarResolver={avatarResolver}
-			usernames={compact ? [] : message.u && message.u.username && [message.u.username]}
+			// eslint-disable-next-line no-nested-ternary
+			usernames={compact ? [] : message.alias ? message.u && message.u.name && [message.u.name] : message.u && message.u.username && [message.u.username]}
 		/>
 		<MessageContent reverse={me}>
 			{renderContent({
