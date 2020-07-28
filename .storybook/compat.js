aefcd19dbc37eb28531bb69d08757b3f98fe9671
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-07-28 17:28:20 -0300

    Remove unused compatibility library

--- .storybook/compat.js
+++ .storybook/compat.js
@@ -1,4 +1,3 @@
 export { default as default }  from 'preact-compat';
 export * from 'preact-compat';
 export { createContext } from 'preact-context';
-export { forwardRef, createRef } from 'create-react-ref';
