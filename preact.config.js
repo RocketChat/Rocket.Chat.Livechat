85993e31d6b8c35e7f87de429a22fc0e27432968
Tasso Evangelista <tasso.evangelista@rocket.chat>
2020-07-28 00:41:30 -0300

    Remove unused modules for testing

--- preact.config.js
+++ preact.config.js
@@ -17,7 +17,7 @@ export default (config, env/* , helpers */) => {
 	delete babelRule.loader;
 	delete babelRule.options;
 
-	const filesRule = config.module.rules.find(({ loader }) => /file-loader/.test(loader));
+	const filesRule = config.module.rules.find(({ loader }) => /file-loader|url-loader/.test(loader));
 	filesRule.test = /\.(woff2?|ttf|eot|jpe?g|png|gif|mp4|mov|ogg|webm)(\?.*)?$/i;
 
 	config.module.rules.push({
