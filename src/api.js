edb4cfd8da229d8e17e47fd8d56ae190795ea6b6
oguhpereira <guhpereira@outlook.com.br>
2020-08-05 21:14:14 -0300

    [FIX] Replace slash at the end host url

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import LivechatClient from '@rocket.chat/sdk/lib/clients/Livechat';
 import queryString from 'query-string';
 
-const host = window.SERVER_URL
+const host = (window.SERVER_URL
 	|| queryString.parse(window.location.search).serverUrl
-	|| (process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : null);
+	|| (process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : null))
+	.replace(/(.*)(\/)/, '$1');
 const useSsl = host && host.match(/^https:/) !== null;
 
 export const Livechat = new LivechatClient({ host, protocol: 'ddp', useSsl });
