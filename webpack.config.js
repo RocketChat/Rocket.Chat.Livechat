44781e14f06b343e58fe51a40eb3e98e878e99ec
Guilherme Gazzo <guilherme@gazzo.xyz>
2020-09-01 15:48:27 -0300

    test ie

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
 		rules: [
 			{
 				test: /\.js$/,
-				exclude: /node_modules/,
+				exclude: /node_modules\/(?!(map-age-cleaner|)\/).*/,
 				use: {
 					loader: 'babel-loader',
 					options: {
