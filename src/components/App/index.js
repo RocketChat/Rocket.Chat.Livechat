a1c2983adad991237f7a2eae2370af6efc744fa6
Renato Becker <renato.augusto.becker@gmail.com>
2021-02-15 13:55:56 -0300

    Add more stories to storkybook.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -110,7 +110,7 @@ export class App extends Component {
 		const dispatchEvent = () => {
 			dispatchRestore();
 			store.off('storageSynced', dispatchEvent);
-		}
+		};
 		if (undocked) {
 			store.on('storageSynced', dispatchEvent);
 		} else {
--- src/routes/TriggerMessage/stories.js
+++ src/routes/TriggerMessage/stories.js
@@ -6,27 +6,35 @@ import { h } from 'preact';
 import { screenCentered, screenProps } from '../../helpers.stories';
 import TriggerMessage from './component';
 
-// const agent = {
-// 	name: 'Guilherme Gazzo',
-// 	status: 'online',
-// 	email: 'guilherme.gazzo@rocket.chat',
-// 	phone: '+55 99 99999 9999',
-// 	username: 'guilherme.gazzo',
-// };
-
 const now = new Date(Date.parse('2021-01-01T00:00:00.000Z'));
 
 const messages = [
-	{ _id: 1, u: { _id: 1, username: 'tasso.evangelista' }, msg: 'Hi there!' },
+	{ _id: 1, u: { _id: 1, username: 'guilherme.gazzo' }, msg: 'Hi There!' },
+	{ _id: 2, u: { _id: 2, username: 'guilherme.gazzo' }, msg: 'Rocket.Chat allows you to chat and create better relationship with your customers on their favorite channels. ' },
+	{ _id: 3, u: { _id: 3, username: 'guilherme.gazzo' }, msg: 'Let us know if you have any question.' },
 ].map((message, i) => ({
 	...message,
 	ts: new Date(now.getTime() - (15 - i) * 60000 - (i < 5 ? 24 * 60 * 60 * 1000 : 0)).toISOString(),
 }));
 
-storiesOf('Routes/TriggerMessages', module)
+storiesOf('Routes/TriggerMessage', module)
 	.addDecorator(screenCentered)
 	.addDecorator(withKnobs)
-	.add('normal', () => (
+	.add('single', () => (
+		<TriggerMessage
+			theme={{
+				color: color('theme/color', ''),
+				fontColor: color('theme/fontColor', ''),
+				iconColor: color('theme/iconColor', ''),
+			}}
+			messages={object('messages', messages.filter(({ _id }) => _id === 3))}
+			title={text('title', '')}
+			onSubmit={action('submit')}
+			onCancel={action('cancel')}
+			{...screenProps()}
+		/>
+	))
+	.add('multiple', () => (
 		<TriggerMessage
 			theme={{
 				color: color('theme/color', ''),
