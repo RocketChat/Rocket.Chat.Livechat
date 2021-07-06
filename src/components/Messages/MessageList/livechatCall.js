8e9199ed5583f5fb3d1cf06184bf77ba167ea764
Deepak Agarwal <deepak710agarwal@gmail.com>
2021-07-06 23:48:56 +0530

    remove-icons

--- src/components/Messages/MessageList/livechatCall.js
+++ src/components/Messages/MessageList/livechatCall.js
@@ -1,8 +1,6 @@
 import { h } from 'preact';
 import { useState } from 'preact/compat';
 
-import PhoneAccept from '../../../icons/phone.svg';
-import PhoneDecline from '../../../icons/phoneOff.svg';
 import { Avatar } from '../../Avatar';
 import { Button } from '../../Button';
 import { Screen } from '../../Screen';
@@ -10,7 +8,7 @@ import { createClassName, getAvatarUrl } from '../../helpers';
 import { parseDate } from '../MessageTime/index';
 import styles from './styles.scss';
 
-// get roomid(GetRoomId(rid)) and put rid id in iframe link
+// get roomid(DisolayIframe(rid)) and put rid id in iframe link
 // <iframe style="height:80%" src="localhost:3000/livechat/roomId"></iframe>
 // For example, for now I have add link of jitsi and its working fine, similarly we can add link of our video/audio call implemented with webrtc.
 const DisplayIframe = (rid) => (
@@ -55,8 +53,8 @@ export const CallNotification = (props) => {
 						/></div>
 					Incoming video Call
 					<div className={createClassName(styles, 'btn')}>
-						<Button onClick={declineClick} className={createClassName(styles, 'btn1')}> <PhoneDecline width={20} height={20} /> Decline</Button>
-						<Button onClick={acceptClick} className={createClassName(styles, 'btn2')} > <PhoneAccept width={20} height={20} />Accept</Button></div></div></Screen.Content>) : null}
+						<Button onClick={declineClick} className={createClassName(styles, 'btn1')}>  Decline</Button>
+						<Button onClick={acceptClick} className={createClassName(styles, 'btn2')} > Accept</Button></div></div></Screen.Content>) : null}
 			{isframe ? (<DisplayIframe session={props.rid} />) : null }
 		</div>);
 };
