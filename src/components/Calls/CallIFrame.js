622439c2d3c47fc45f759ef3159bcb5c6acb440e
Dhruv Jain <dhruv.jain93@gmail.com>
2021-08-14 01:30:55 +0530

    [NEW] Handle endCall and expandView control buttons

--- src/components/Calls/CallIFrame.js
+++ src/components/Calls/CallIFrame.js
@@ -8,7 +8,17 @@ import styles from './styles.scss';
 
 export const CallIframe = () => {
 	const { token, room } = store.state;
-	const url = `${ Livechat.client.host }/meet/${ room._id }?token=${ token }`;
+	const url = `${Livechat.client.host}/meet/${room._id}?token=${token}&layout=embedded`;
+	window.handleIframeClose = async () => {
+		return await store.setState({ incomingCallAlert: null });
+	};
+	window.expandCall = async () => {
+		window.open(
+			`${Livechat.client.host}/meet/${room._id}?token=${token}`,
+			room._id
+		);
+		return await store.setState({ incomingCallAlert: null });
+	};
 	return (
 		<div className={createClassName(styles, 'call-iframe')}>
 			<iframe className={createClassName(styles, 'call-iframe__content')} allow='camera;microphone' src={url} />
