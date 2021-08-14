788698bb27ca2859a63807ada4910a6242bbaf98
Dhruv Jain <dhruv.jain93@gmail.com>
2021-08-14 11:56:57 +0530

    [FIX] Lint errors

--- src/components/Calls/CallIFrame.js
+++ src/components/Calls/CallIFrame.js
@@ -8,16 +8,14 @@ import styles from './styles.scss';
 
 export const CallIframe = () => {
 	const { token, room } = store.state;
-	const url = `${Livechat.client.host}/meet/${room._id}?token=${token}&layout=embedded`;
-	window.handleIframeClose = async () => {
-		return await store.setState({ incomingCallAlert: null });
-	};
+	const url = `${ Livechat.client.host }/meet/${ room._id }?token=${ token }&layout=embedded`;
+	window.handleIframeClose = () => store.setState({ incomingCallAlert: null });
 	window.expandCall = async () => {
 		window.open(
-			`${Livechat.client.host}/meet/${room._id}?token=${token}`,
-			room._id
+			`${ Livechat.client.host }/meet/${ room._id }?token=${ token }`,
+			room._id,
 		);
-		return await store.setState({ incomingCallAlert: null });
+		return store.setState({ incomingCallAlert: null });
 	};
 	return (
 		<div className={createClassName(styles, 'call-iframe')}>
