b48cbec639ba9631eca92722d363d67ace831a09
Dhruv Jain <dhruv.jain93@gmail.com>
2021-08-16 01:07:04 +0530

    [REF] Use spread operator, set ongoingCall status when expanding

--- src/components/Calls/CallIFrame.js
+++ src/components/Calls/CallIFrame.js
@@ -9,25 +9,16 @@ import styles from './styles.scss';
 export const CallIframe = () => {
 	const { token, room, incomingCallAlert } = store.state;
 	const url = `${ Livechat.client.host }/meet/${ room._id }?token=${ token }&layout=embedded`;
-	window.handleIframeClose = () => store.setState({ incomingCallAlert: {
-		show: false,
-		callProvider: incomingCallAlert.callProvider,
-		callerUsername: incomingCallAlert.callerUsername,
-		rid: incomingCallAlert.rid,
-		time: incomingCallAlert.time,
-	} });
+	window.handleIframeClose = () => store.setState({ incomingCallAlert: { ...incomingCallAlert, show: false } });
 	window.expandCall = () => {
 		window.open(
 			`${ Livechat.client.host }/meet/${ room._id }?token=${ token }`,
 			room._id,
 		);
-		return store.setState({ incomingCallAlert: {
-			show: false,
-			callProvider: incomingCallAlert.callProvider,
-			callerUsername: incomingCallAlert.callerUsername,
-			rid: incomingCallAlert.rid,
-			time: incomingCallAlert.time,
-		} });
+		return store.setState({
+			incomingCallAlert: { ...incomingCallAlert, show: false },
+			ongoingCall: { ...ongoingCall, callStatus: "ongoingCallInNewTab" },
+		});
 	};
 	return (
 		<div className={createClassName(styles, 'call-iframe')}>
