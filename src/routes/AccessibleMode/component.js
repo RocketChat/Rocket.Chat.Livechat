24b7fffc3e7c9b134c8c2659a85fc9bce0ed8187
ghulamghousdev <itsghulamghous@gmail.com>
2021-08-18 16:53:21 +0500

    Added manual fonts, changed dynamictext selection panel, fixed lint errors

--- src/routes/AccessibleMode/component.js
+++ src/routes/AccessibleMode/component.js
@@ -2,10 +2,9 @@ import { h, Component } from 'preact';
 
 import { Button } from '../../components/Button';
 import { ButtonGroup } from '../../components/ButtonGroup';
+import { Form } from '../../components/Form';
 import Screen from '../../components/Screen';
 import Toggle from '../../components/ToggleSwitch';
-import { Form, FormField, SelectInput, Validations } from '../../components/Form';
-import {setDarkModeState} from '../../lib/main'
 import { createClassName } from '../../components/helpers';
 import I18n from '../../i18n';
 import styles from './styles.scss';
@@ -14,31 +13,26 @@ const defaultTitle = I18n.t('Accessible Mode');
 const darkModeText = I18n.t('Dark Mode');
 const iconAccompanyingText = I18n.t('Icons Accompanying Text');
 const dynamicText = I18n.t('Text Size');
-const textSize = I18n.t('textSize');
-
-const optionsArray =[{_id: 1, name:"small"}, {_id:2, name: "normal"}, {_id:2, name: "large"}]
 
 export default class AccessibleMode extends Component {
-
 	handleBackClick = () => {
 		const { onCancel } = this.props;
 		onCancel && onCancel();
 	}
 
-	constructor(props) {
-		super(props);
-	}
-	
-	render({ 
-		title, 
-		color, 
-		loading, 
-		dynamicTextState, 
-		darkModeState, 
-		iconsAccompanyingTextState, 
-		setIconsAccompanyingTextState, 
+	render({
+		title,
+		color,
+		loading,
+		dynamicTextState,
+		darkModeState,
+		iconsAccompanyingTextState,
+		setIconsAccompanyingTextState,
 		setDarkModeState,
-		...props 
+		setSmallDynamicTextState,
+		setNormalDynamicTextState,
+		setLargeDynamicTextState,
+		...props
 	}) {
 		return (
 			<Screen
@@ -46,30 +40,42 @@ export default class AccessibleMode extends Component {
 				title={title || defaultTitle}
 				className={createClassName(styles, 'accessible-mode')}
 				iconsAccompanyingText={iconsAccompanyingTextState}
+				dynamicTextState={dynamicTextState}
 				{...props}
 			>
-				
+
 				<Screen.Content>
 					<Form>
-					<Toggle title={darkModeText} handleToggleSwitch={setDarkModeState} modeToggled={darkModeState}/>		
-					<Toggle title={iconAccompanyingText} handleToggleSwitch={setIconsAccompanyingTextState} modeToggled={iconsAccompanyingTextState}/>
-					<p className={createClassName(styles, 'toggle-switch__title')}>{dynamicText}</p>	
-					<FormField>
-						<SelectInput
-							name={textSize}
-							value={this.state.dynamicText}
-							options={optionsArray.map(({ _id, name }) => ({ value: _id, label: name }))}
-							placeholder={I18n.t('Choose a Font Size...')}
-							disabled={loading}
-						/>
-					</FormField>
-
-					<ButtonGroup>
-						<Button submit loading={loading} disabled={loading} stack onClick={this.handleBackClick}>{I18n.t('Back')}</Button>
-					</ButtonGroup>
+						<Toggle title={darkModeText} handleToggleSwitch={setDarkModeState} modeToggled={darkModeState} dynamicText={dynamicTextState} />
+						<Toggle title={iconAccompanyingText} handleToggleSwitch={setIconsAccompanyingTextState} modeToggled={iconsAccompanyingTextState} dynamicText={dynamicTextState} />
+						<div className={createClassName(styles, 'dynamic-content-row')}>
+							<p className={createClassName(styles, 'toggle-switch__title')}>
+								<span className={createClassName(styles, `dynamic-text-title-${ dynamicTextState }`)}>{dynamicText}</span>
+							</p>
+							<div className={createClassName(styles, 'buttons-group')}>
+								<button className={createClassName(styles, 'dynamic-font')} onClick={setSmallDynamicTextState}>
+									<div className={createClassName(styles, `fourteen-${ dynamicTextState }`)}>
+										<p className={createClassName(styles, 'dynamic-font__small-button')}>{I18n.t('Aa')}</p>
+									</div>
+								</button>
+								<button className={createClassName(styles, 'dynamic-font')} onClick={setNormalDynamicTextState}>
+									<div className={createClassName(styles, `twenty-${ dynamicTextState }`)}>
+										<p className={createClassName(styles, 'dynamic-font__medium-button')}>{I18n.t('Aa')}</p>
+									</div>
+								</button>
+								<button className={createClassName(styles, 'dynamic-font')} onClick={setLargeDynamicTextState}>
+									<div className={createClassName(styles, `twentyfour-${ dynamicTextState }`)}>
+										<p className={createClassName(styles, 'dynamic-font__large-button')}>{I18n.t('Aa')}</p>
+									</div>
+								</button>
+							</div>
+						</div>
+						<ButtonGroup>
+							<Button submit loading={loading} disabled={loading} stack onClick={this.handleBackClick}>{I18n.t('Back')}</Button>
+						</ButtonGroup>
 					</Form>
-				</Screen.Content>  
-			
+				</Screen.Content>
+				<Screen.Footer />
 			</Screen>
 		);
 	}
