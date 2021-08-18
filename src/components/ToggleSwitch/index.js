8d558b6fa14164737f048f3dd6c71200efcd3c2e
ghulamghousdev <itsghulamghous@gmail.com>
2021-08-18 16:59:22 +0500

    Lint errors fixed

--- src/components/ToggleSwitch/index.js
+++ src/components/ToggleSwitch/index.js
@@ -1,22 +1,25 @@
-import { h,Fragment, Component } from "preact";
-import { createClassName} from '../helpers';
-import styles from "./styles.scss";
+/* eslint-disable react/prefer-stateless-function */
+import { h, Component } from 'preact';
+
+import { createClassName } from '../helpers';
+import styles from './styles.scss';
 
 export default class Toggle extends Component {
-
-  render({title, handleToggleSwitch, modeToggled}) {
-    return (
-      <div className={createClassName(styles, 'toggle-switch')}>
-        <p className={createClassName(styles, 'toggle-switch__title')}>{title}</p>
-        <label className={createClassName(styles, 'toggleswitch')}>
-            <input
-            type="checkbox"
-            checked={modeToggled}
-            onChange={(e) => handleToggleSwitch(e)}
-            />
-            <span className={createClassName(styles, 'switch')} />
-        </label>
-        </div>
-    );
-  }
+	render({ title, handleToggleSwitch, modeToggled, dynamicText }) {
+		return (
+			<div className={createClassName(styles, 'toggle-switch')}>
+				<p className={createClassName(styles, 'toggle-switch__title')}>
+					<span className={createClassName(styles, `toggle-switch-title-${ dynamicText }`)}>{title}</span>
+				</p>
+				<label className={createClassName(styles, 'toggleswitch')}>
+					<input
+						type='checkbox'
+						checked={modeToggled}
+						onChange={(e) => handleToggleSwitch(e)}
+					/>
+					<span className={createClassName(styles, 'switch')} />
+				</label>
+			</div>
+		);
+	}
 }
