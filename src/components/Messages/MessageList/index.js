bfa0afe2ef3b2dae70b1febeaa6a7e5c509c0225
murtaza98 <murtaza.patrawala@rocket.chat>
2021-09-14 18:01:52 +0530

    [NEW] add-jitsi-call-support

--- src/components/Messages/MessageList/index.js
+++ src/components/Messages/MessageList/index.js
@@ -2,6 +2,7 @@ import { parseISO } from 'date-fns/fp';
 import isSameDay from 'date-fns/isSameDay';
 import { h } from 'preact';
 
+import constants from '../../../lib/constants';
 import store from '../../../store';
 import { CallTime } from '../../Calls/CallTime';
 import { JoinCallButton } from '../../Calls/JoinCallButton';
@@ -116,7 +117,7 @@ export class MessageList extends MemoizedComponent {
 			const { incomingCallAlert } = store.state;
 			const { ongoingCall } = store.state;
 
-			if (message.actionLinks && message.actionLinks.length === 2 && ongoingCall) {
+			if ((message.t === constants.webrtcCallStartedMessageType || message.t === constants.jitsiCallStartedMessageType) && message.actionLinks && message.actionLinks.length > 0 && ongoingCall) {
 				const { url, callProvider, rid } = incomingCallAlert || {};
 				items.push(
 					<JoinCallButton callStatus={ongoingCall.callStatus} url={url} callProvider={callProvider} rid={rid} />,
