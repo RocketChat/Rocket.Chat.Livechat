6044de7469b66bf21c2c9eda20f24a113b3fefdc
Tiago Evangelista Pinto <tiago.evangelista@rocket.chat>
2021-10-24 02:01:35 -0300

    i18next setup

---
+++ src/i18next.js
@@ -0,0 +1,31 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import { browserLanguage, configLanguage, normalizeLanguageString } from './lib/locale';
+
+export const loadResource = async (lng) => {
+	const { default: { translation } } = await import(`./i18next/${lng}.json`);
+
+	return translation;
+};
+
+export default i18next
+		.use({
+			type: 'backend',
+			read: (language, namespace, callback) => {
+				loadResource(language).then((resource) => {
+					callback(null, resource);
+				}).catch((reason) => {
+					callback(reason, null);
+				});
+			}
+		})
+		.use(initReactI18next)
+		.init({
+			lng: normalizeLanguageString(configLanguage() || browserLanguage()),
+			fallbackLng: 'en',
+			debug: true,
+			react: {
+				useSuspense: false,
+			}
+		});
