01cc8c7ee5ad8ae0220021cc56d36a7b9bf99ebb
Kevin Aleman <kevin.aleman@rocket.chat>
2021-12-02 13:04:03 -0600

    remove unused change

--- src/store/Store.js
+++ src/store/Store.js
@@ -77,10 +77,6 @@ export default class Store {
 		this.emit('change', [this._state, prevState, partialState]);
 	}
 
-	cleanState() {
-		this._state = {};
-	}
-
 	setStoredState(storedState) {
 		const prevState = this._state;
 
