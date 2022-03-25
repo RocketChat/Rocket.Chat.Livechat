716c285e0681572a8e60d15259d3efffec45f563
Cauê Felchar <11652381+cauefcr@users.noreply.github.com>
2022-03-25 10:08:26 -0300

    moved constants out of the function

--- src/lib/email.js
+++ src/lib/email.js
@@ -1,7 +1,7 @@
-export const validateEmail = (email, options = { style: 'basic' }) => {
-	const basicEmailRegex = /^[^@]+@[^@]+$/;
-	const rfcEmailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+const basicEmailRegex = /^[^@]+@[^@]+$/;
+const rfcEmailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
+export const validateEmail = (email, options = { style: 'basic' }) => {
 	switch (options.style) {
 		case 'rfc':
 			return rfcEmailRegex.test(email);
