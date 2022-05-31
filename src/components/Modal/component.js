374a3d89c8763910096bf70fcc376c74c99bb552
Fabio Surrage <fabiosurrage@gmail.com>
2022-05-31 16:47:37 -0300

    Add focus on close chat modal

--- src/components/Modal/component.js
+++ src/components/Modal/component.js
@@ -74,6 +74,14 @@ export class ConfirmationModal extends Component {
 		this.confirmationModalRef = ref;
 	}
 
+	handleCancel = () => {
+		const { focusRef, onCancel } = this.props;
+		if (focusRef) {
+			focusRef.focus();
+		}
+		onCancel();
+	}
+
 	handleKeyDown = (event) => {
 		const { key } = event;
 
@@ -82,7 +90,7 @@ export class ConfirmationModal extends Component {
 				handleTabKey(event, this.confirmationModalRef.base);
 				break;
 			case 'Escape':
-				this.props.onCancel();
+				this.handleCancel();
 				break;
 			default:
 				break;
@@ -107,7 +115,7 @@ export class ConfirmationModal extends Component {
 		<Modal open animated dismissByOverlay={false} onkeydown={this.handleKeyDown} ref={this.handleRef} role='dialog' aria-describedby='chat-confirmation-modal' {...props}>
 			<Modal.Message>{text}</Modal.Message>
 			<ButtonGroup>
-				<Button outline secondary onClick={onCancel}>{cancelButtonText || i18next.t('no')}</Button>
+				<Button outline secondary onClick={this.handleCancel}>{cancelButtonText || i18next.t('no')}</Button>
 				<Button danger onClick={onConfirm}>{confirmButtonText || i18next.t('yes')}</Button>
 			</ButtonGroup>
 		</Modal>
