218b07603b89e6b09eaaf087ad1fe91e809606ec
Fabio Surrage <fabiosurrage@gmail.com>
2022-06-07 16:24:33 -0300

    Fix first focus on input element

--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -125,6 +125,7 @@ export class Screen extends Component {
 
 	handleOnRestore = () => {
 		this.props.onRestore();
+		this.handleFirstElementFocused();
 		addFocusFirstElement(this.screenRef);
 	}
 
@@ -133,9 +134,18 @@ export class Screen extends Component {
 		this.buttonRef.base.focus();
 	}
 
+	handleFirstElementFocused = () => {
+		const inputElement = this.screenRef.querySelector('div[contenteditable="true"]');
+		if (inputElement) {
+			inputElement.focus();
+		} else {
+			addFocusFirstElement(this.screenRef);
+		}
+	}
+
 	componentDidMount() {
 		if (!this.props.minimized && !this.props.windowed && this.state.opened !== !this.props.minimized) {
-			addFocusFirstElement(this.screenRef);
+			this.handleFirstElementFocused();
 		}
 
 		if (this.state.opened !== !this.props.minimized) {
